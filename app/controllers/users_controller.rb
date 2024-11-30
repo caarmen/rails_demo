a7774ca8e444fd237a2d19b62eaefcf6ec80eab6
Carmen Alvarez <carmen@rmen.ca>
2024-11-30 23:41:02 +0100

    Show the user count.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def index
     @sort_field = params[:sort].presence_in(%w[name age updated_at]) || 'name'
     @users = User.order(@sort_field => :asc)
+    @count = @users.size
   end
 end
