01913930ff64ca99e8ec0aa29523fac9492e69df
Carmen Alvarez <carmen@rmen.ca>
2024-11-30 17:01:56 +0100

    Run "bundle update"

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_30_155002) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_30_155002) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "age"
   end
-
 end
