9bbab5db7e88d6a1f9564d1d48729ee24a2463fa
Carmen Alvarez <carmen@rmen.ca>
2024-11-30 23:45:12 +0100

    Sort by updated_at desc, but by other fields asc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def index
     @sort_field = params[:sort].presence_in(%w[name age updated_at]) || 'name'
-    @users = User.order(@sort_field => :asc)
+    sort_order = @sort_field == 'updated_at' ? :desc : :asc
+    @users = User.order(@sort_field => sort_order)
     @count = @users.size
   end
 end
